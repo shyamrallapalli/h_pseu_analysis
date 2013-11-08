5884214ee65915536bb4224e4920181d8d95a5a6
Shyam Rallapalli <ghanasyam.rallapalli@tsl.ac.uk>
2013-11-08 14:59:41 +0000

    Get descriptions script updated to get Go id domain
    Go id domains are appended to before the description for each
    respective term
    Process=> Biological Process
    Function=> Molecular Function
    Component=> Cellular Component

--- go_analysis/get_descriptions.rb
+++ go_analysis/get_descriptions.rb
@@ -13,32 +13,31 @@ require 'bio'
 require 'barmcakes'
 require 'net/http'
 
-#def write_gaf
-  
-#end
-
-data = Hash.new {|h,k| h[k] = {} }
-all_terms = []
+#all_terms = []
 
 File.open(ARGV[0]).each do |line|
   next unless line =~ /\w+/
+  data = Hash.new {|h,k| h[k] = {} }
   result = JSON.parse(line.chomp.chop)
   gene = result.keys.first
   result[gene].each do |goterm|
     begin
       next unless goterm.length > 3
-      s = Net::HTTP.get('www.ebi.ac.uk', "/QuickGO/GAnnotation?format=tsv&goid=#{goterm}&col=goName&limit=1&termUse=slim")
+      s = Net::HTTP.get('www.ebi.ac.uk', "/QuickGO/GAnnotation?format=tsv&goid=#{goterm}&col=aspect&col=goName&limit=1&termUse=slim")
+      # guide lines are at http://www.ebi.ac.uk/QuickGO/WebServices.html
       desc = s.chomp.split("\n").last
-      all_terms << goterm
+      desc = (desc.chomp.split("\t")).join(";")
+#      all_terms << goterm
       #puts ["UniProtKB",gene,gene,goterm,'PMID:12345','IEA','F','protein','57401','20090118','DM1'].join("\t")
       data[gene][goterm] = desc
     rescue
       $stderr.puts "meh..."
-    end
+    end  
   end
+  next unless data != {}
+  File.open("terms_and_descriptions.json", "a+").write("#{data.to_json},\n")
 end
 
-File.open("terms_and_descriptions.json", "w").write(data.to_json)
 #File.open("list_of_terms.txt", "w").write( all_terms.join("\n") )
 
-#write_gaf all_terms
+# http://www.ebi.ac.uk/QuickGO/GAnnotation?format=tsv&goid=GO:0006355&col=aspect&col=goName&limit=1&termUse=slim
